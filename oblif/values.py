0393afd62f8d240fb3d082de532227b7130a2cac
Meilof Veeningen <meilof@gmail.com>
2021-05-12 19:47:18 +0200

    simplified context without deepcopy

--- oblif/values.py
+++ oblif/values.py
@@ -17,47 +17,69 @@ class cachedifthenelse:
 class cor_dict:
     def __init__(self, dic):
         self.dic = dic
-        self.reads = set()
         
     def __getitem__(self, var):
-        if not var in self.reads:
-#            print("deepcopying", var)
-            self.dic[var] = deepcopy(self.dic[var])
-            self.reads.add(var)
-            if isinstance(self.dic[var], cachedifthenelse): self.dic[var]=self.dic[var]()
+        if isinstance(self.dic[var], cachedifthenelse): self.dic[var]=self.dic[var]()
         return self.dic[var]
     
     def __setitem__(self, var, val):
         self.dic[var] = val
-        self.reads.add(var)
         
     def __repr__(self):
-        return "{" + ", ".join([repr(nm)+": " + repr(self.dic[nm]) for nm in self.reads]) + "}"
+        return repr(self.dic)
     
 def apply_to_label(contexts, vals, cond, label):
 #        print_ctx("applying to label", label)
     if not label in contexts:
 #            print_ctx("first time, setting dict to", vals)
-        contexts[label] = cor_dict(dict(vals.dic))
-        contexts[label]["__guard"] &= cond
-#            if isinstance(cond, bool):
-#                # do nto need to deepcopy if we have an unconditional jump
-#                self.contexts[label].reads = set(vals.reads)
+        if cond is True:
+            contexts[label] = vals
+            return None
+        else:
+            contexts[label] = cor_dict(dict(vals.dic))
+            contexts[label]["__guard"] &= cond
+            vals["__guard"] &= (1-cond)
+            return vals
     else:
-        guard = vals["__guard"] & cond
-#            print_ctx("oblivious merge")
-#            print_ctx("orig: ", self.contexts[label])
-#            print_ctx("new: ", cond, guard, vals)
-        # merge the two contexts
-        # if we are here, we are guaranteed that cond must be oblivious,
-        # otherwise we could not arrive at the label in two different ways
-        nwcor = cor_dict({})
-        #nwctx = dict()
-        for nm in contexts[label].dic:
-            if nm in vals.dic:
-                nwcor.dic[nm] = cachedifthenelse(guard, vals.dic[nm], contexts[label].dic[nm])
-        contexts[label] = nwcor #cor_dict(nwctx)
-#            print_ctx("result of merge:", nwctx)
+        if cond is True:
+            guard1 = vals["__guard"]
+
+            d1old = contexts[label].dic
+            d2old = vals.dic
+            d1new = {}
+
+            for nm in d1old:
+                if nm in d2old:
+                    if d1old[nm] is d2old[nm]:
+                        d1new[nm] = d1old[nm]
+                    else:
+                        d1new[nm] = cachedifthenelse(guard1, d2old[nm], d1old[nm])
+            d1new["__guard"] = d1old["__guard"]|d2old["__guard"]
+            contexts[label].dic = d1new            
+            return None
+        else:
+            guard1 = vals["__guard"] & cond
+            guard2 = vals["__guard"] & (1-cond)
+            
+            d1old = contexts[label].dic
+            d2old = vals.dic
+            d1new = {}
+            d2new = {}
+
+            for nm in d1old:
+                if nm in d2old:
+                    if d1old[nm] is d2old[nm]:
+                        d1new[nm] = d1old[nm]
+                        d2new[nm] = d1old[nm]
+                    else:
+                        d1new[nm] = cachedifthenelse(guard1, d2old[nm], d1old[nm])
+                        d2new[nm] = d2old[nm]
+
+            d1new["__guard"] = guard1
+            d2new["__guard"] = guard2
+            contexts[label].dic = d1new
+            return cor_dict(d2new)
+
 
 def values_new():
     return cor_dict({})
