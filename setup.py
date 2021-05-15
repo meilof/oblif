1318d91224f355ec69ffb1d4e85cc0c00643c0af
Meilof Veeningen <meilof@gmail.com>
2021-05-15 21:10:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='oblif',
-      version='0.1.0',
+      version='0.2.0',
       description='Bytecode transformations to produce data-oblivious code',
       author='Meilof Veeningen',
       author_email='meilof@gmail.com',
